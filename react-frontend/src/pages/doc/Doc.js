ed20fc2eedcdc294edd546337c751e40f15933bc
Ramya Peravali <ramyaperavali136@gmail.com>
2021-02-12 19:04:55 +0530

    doc images added

--- react-frontend/src/pages/doc/Doc.js
+++ react-frontend/src/pages/doc/Doc.js
@@ -43,7 +43,7 @@ const Doc =  () => {
                         </li>
                         <li>
                             <div class="timeline-content">
-                                <h6>Download ssd_mobilenet_v2_fpnlite_320x320_coco17_tpu-8.tar.gz </h6>
+                                <h6>Download ssd_mobilenet_v2_fpnlite_320x320_coco17_tpu-8.tar.gz</h6>
                             </div>
                         </li>
                         <li>
